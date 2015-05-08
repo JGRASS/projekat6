8e3ea26bf717cf4401c3363e4defc63f42305e35
Tatjana <vasiljevictatjana@yahoo.com>
2015-05-08 18:24:08 +0200

    Napravljena klasa Student sa set i get metodama

--- src/planner/Student.java
+++ src/planner/Student.java
@@ -1,5 +1,34 @@
 package planner;
 
 public class Student {
+	
+	private String ime;
+	private String prezime;
+	private String brIndexa;
+	
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		if(ime==null)
+			throw new RuntimeException("Ime ne sme biti null.");
+		this.ime = ime;
+	}
+	public String getPrezime() {
+		return prezime;
+	}
+	public void setPrezime(String prezime) {
+		if(prezime==null)
+			throw new RuntimeException("Prezime ne sme biti null.");
+		this.prezime = prezime;
+	}
+	public String getBrIndexa() {
+		return brIndexa;
+	}
+	public void setBrIndexa(String brIndexa) {
+		if(brIndexa==null)
+				throw new RuntimeException("Broj indeksa ne sme biti null.");
+		this.brIndexa = brIndexa;
+	}
 
 }
